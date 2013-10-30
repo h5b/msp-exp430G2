a50ae797be14c371dfe05a4980a04a3989290ed4
Sebastian Trahm <basti@schleifi.com>
2013-10-30 22:15:42 +0100

    Install msp430-Toolchain via Chef.
    * Add recipe "msp430dev" to Vagrantfile
      o Install Basic Development Packages
      o Install msp430-Toolchain Packages

---
+++ chef/cookbooks/msp430dev/recipes/default.rb
@@ -0,0 +1,20 @@
+Chef::Log.info("[Adding: Basic Development Packages]")
+[
+  "build-essential",
+  "git-core",
+  "vim",
+].each do |p|
+  package p
+end
+
+Chef::Log.info("[Adding: msp430-Toolchain Packages]")
+[
+  "binutils-msp430",
+  "gcc-msp430",
+  "gdb-msp430",
+  "msp430-libc",
+  "msp430mcu",
+  "mspdebug",
+].each do |p|
+  package p
+end
